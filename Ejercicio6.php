854ac3384d0c33f1ddb8d44599ee23fdfd87d89a
uo226650 <uo226650@uniovi.es>
2020-12-17 16:17:10 +0100

    viewport

--- Ejercicio6.php
+++ Ejercicio6.php
@@ -3,6 +3,7 @@
 <head>
     <title>BaseDatos :: PruebasUsabilidad</title>
     <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Rocío Cenador Martínez" /> 
     <link href="Ejercicio6.css" rel="stylesheet" />
 </head>   
